2c24f64891f44c7d14b0c9c893158f05be6a5158
Your Name <mishra.abhi9619@gmail.com>
2021-02-18 17:01:38 +0530

    apply multithreading for encryption

--- nuagecipher.py
+++ nuagecipher.py
@@ -1,6 +1,7 @@
 import string
 import threading
 import sys
+import time
 
 num = string.digits
 punct = string.punctuation
@@ -16,7 +17,7 @@ Cipher_data = {
       } 
 
 def EncrypFunct(message):
-	print(Cipher_data)
+	print(message)
 	output = ''
 	for i in message:
 		if (i != ' ' and i not in punct and i not in num):
@@ -35,21 +36,22 @@ def FileOpenRead():
 	global read_data
 	with open("input_data.txt", "r") as f:
 		read_data = EncrypFunct(f.read().rstrip("\n"))
-	
-t = threading.Thread(target = FileOpenRead())
-t.start()
-t.join()
 
-output = read_data[0].upper() + read_data[1:]
-encrypted_data = []
-for i in output.split(' '):
-
-	if (i.startswith('w')):
-		encrypted_data.append(i[0].upper() + i[1:])
-
-	elif not (i.startswith('w')):
-		encrypted_data.append(i)
-		
-with open("output_data.txt", "w") as f:
-	f.write(' '.join(encrypted_data))
-
+t1 = threading.Thread(target = FileOpenRead())
+t2 = threading.Thread(target = EncrypFunct, args=(Cipher_data,) ,daemon=True)
+t1.start()
+t2.start()
+t1.join()
+t2.join()	
+try:
+	output = read_data[0].upper() + read_data[1:]
+	encrypted_data = []
+	for i in output.split(' '):
+		if (i.startswith('w')):
+			encrypted_data.append(i[0].upper() + i[1:])
+		elif not (i.startswith('w')):
+			encrypted_data.append(i)
+			with open("output_data.txt", "w") as f:
+				f.write(' '.join(encrypted_data))
+except:
+  print("Exception:Index out of range")
